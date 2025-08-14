a4d2dad23a74ae4413214898d3ee26663ec943cf
Bansi-sanskar <bansi@sanskartechnolab.com>
2025-08-14 19:08:01 +0530

    [Feat] : Code Changes for taxe table in sales invoice

--- posawesome/posawesome/api/invoice.js
+++ posawesome/posawesome/api/invoice.js
@@ -9,4 +9,36 @@ frappe.ui.form.on('Sales Invoice', {
             };
         });
     },
+    before_save: function(frm) {
+        if (frm.doc.taxes && frm.doc.taxes.length > 0) {
+
+            let lastRowIdx = frm.doc.taxes.length; 
+            if(frm.doc.taxes[0].description === 'VAT' && frm.doc.taxes[frm.doc.taxes.length - 1].charge_type !== 'On Previous Row Total'){
+                let firstTaxRow = frm.doc.taxes[0];
+                frm.doc.taxes.splice(0, 1);
+                firstTaxRow.charge_type = 'On Previous Row Total';
+                frm.doc.taxes.push(firstTaxRow);
+
+            }
+            
+            frm.doc.taxes[frm.doc.taxes.length - 1].row_id = lastRowIdx - 1
+    
+            frm.doc.taxes.forEach(function(row, index) {
+                row.idx = index + 1;
+            });
+    
+            let idxSet = new Set();
+            frm.doc.taxes.forEach(function(row) {
+                if (!row.idx) {
+                    frappe.throw("Row ID is missing for some tax rows.");
+                }
+                if (idxSet.has(row.idx)) {
+                    frappe.throw(`Duplicate Row ID found: ${row.idx}`);
+                }
+                idxSet.add(row.idx);
+            });
+
+            frm.refresh_field('taxes');
+        }
+    }
 });
